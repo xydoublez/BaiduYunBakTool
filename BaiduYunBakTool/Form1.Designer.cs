18893a3ddb547a0c81d0d051c8fbf4d5caaefcf1
李志强 <love@lizhiqiang.name>
2017-08-23 12:54:09 +0800

    添加项目文件。

---
+++ BaiduYunBakTool/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace BaiduYunBakTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.打开主窗体ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出程序ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(92, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 58);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "备份";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.打开主窗体ToolStripMenuItem,
+            this.退出程序ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(137, 48);
+            // 
+            // 打开主窗体ToolStripMenuItem
+            // 
+            this.打开主窗体ToolStripMenuItem.Name = "打开主窗体ToolStripMenuItem";
+            this.打开主窗体ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.打开主窗体ToolStripMenuItem.Text = "打开主窗体";
+            this.打开主窗体ToolStripMenuItem.Click += new System.EventHandler(this.打开主窗体ToolStripMenuItem_Click);
+            // 
+            // 退出程序ToolStripMenuItem
+            // 
+            this.退出程序ToolStripMenuItem.Name = "退出程序ToolStripMenuItem";
+            this.退出程序ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.退出程序ToolStripMenuItem.Text = "退出程序";
+            this.退出程序ToolStripMenuItem.Click += new System.EventHandler(this.退出程序ToolStripMenuItem_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipText = "PACS图像转存服务，不可退出！";
+            this.notifyIcon1.BalloonTipTitle = "提示";
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "PACS图像转存服务，不可退出！";
+            this.notifyIcon1.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "知识库备份文件到百度云";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 打开主窗体ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出程序ToolStripMenuItem;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ BaiduYunBakTool/Form1.cs
@@ -0,0 +1,194 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using static BaiduYunBakTool.API;
+using static BaiduYunBakTool.win32;
+
+namespace BaiduYunBakTool
+{
+    public partial class Form1 : Form
+    {
+        string StartTime = ConfigurationManager.AppSettings["StartTime"].ToString();
+        string WinRarPath = ConfigurationManager.AppSettings["WinRarPath"].ToString();
+        string IISPath = ConfigurationManager.AppSettings["IISPath"].ToString();
+        string MysqlPath = ConfigurationManager.AppSettings["MysqlPath"].ToString();
+        string User = ConfigurationManager.AppSettings["User"].ToString();
+        string Password = ConfigurationManager.AppSettings["Password"].ToString();
+        string DbName = ConfigurationManager.AppSettings["DbName"].ToString();
+        string BakPath = ConfigurationManager.AppSettings["BakPath"].ToString();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (DateTime.Now.ToString("HH:mm:ss") == ConfigurationManager.AppSettings["StartTime"].ToString() + ":00")
+            {
+                Bak();
+            }
+        }
+        private void Bak()
+        {
+            if (!Directory.Exists(BakPath))
+            {
+                Directory.CreateDirectory(BakPath);
+            }
+            BakPath = BakPath + DateTime.Now.ToString("yyyyMMdd");
+            if (!Directory.Exists(BakPath))
+            {
+                Directory.CreateDirectory(BakPath);
+            }
+            //GenMysqlBak();
+            //GenIISBak();
+            Thread.Sleep(1000);
+            uploadBaidu();
+
+        }
+        private void GenMysqlBak()
+        {
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.FileName = Path.Combine(MysqlPath, "mysqldump.exe");
+            info.WindowStyle = ProcessWindowStyle.Hidden;
+            info.Arguments = "-u" + User + " " + "-p" + Password + " " + DbName + " > " + Path.Combine(BakPath, DateTime.Now.ToString("yyyyMMddHHmm") + ".sql");
+            Process p = Process.Start(info);
+            p.WaitForExit(1000 * 60 * 10);
+
+        }
+        private void GenIISBak()
+        {
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.FileName = Path.Combine(WinRarPath, "Rar.exe");
+            info.WindowStyle = ProcessWindowStyle.Hidden;
+            info.Arguments = " a " + Path.Combine(BakPath, DateTime.Now.ToString("yyyyMMddHHmm") + ".rar") + " " + IISPath;
+            Process p = Process.Start(info);
+            p.WaitForExit(1000 * 60 * 10);
+        }
+        private void uploadBaidu()
+        {
+            //查找baidu窗口句柄
+            var Shell_TrayWnd = win32.FindWindow("Shell_TrayWnd", null);
+            IntPtr hNext = IntPtr.Zero;
+            //第一个对话框
+            var TrayNotifyWnd = win32.FindWindowEx(Shell_TrayWnd, hNext, "TrayNotifyWnd", "");
+            var SysPager = win32.FindWindowEx(TrayNotifyWnd, hNext, "SysPager", "");
+            var ToolbarWindow32 = win32.FindWindowEx(SysPager, hNext, "用户升级的通知区域", "");
+            TBBUTTONINFO info = new TBBUTTONINFO();
+            Marshal.PtrToStructure(API.SendMessage(ToolbarWindow32, API.TB_GETBUTTON, 0, 0), info);
+            Rectangle IconRect = new Rectangle();
+            API.SendMessage(ToolbarWindow32, API.TB_GETRECT, info.idCommand, ref IconRect);
+            Rectangle barRect = new Rectangle();
+            API.GetWindowRect(ToolbarWindow32, ref barRect);
+            //将像素坐标转化为绝对坐标：
+            //API中MouseInput结构中的dx，dy含义是绝对坐标，是相对屏幕的而言的，屏幕左上角的坐标为（0,0），右下角的坐标为（65535,65535）。而我们在C#中获得的对象（Frame，button，flash等）的坐标都是像素坐标，是跟你当前屏幕的分辨率相关的。假如你的显示器分辨率是1024*768，那么屏幕左上角的像素坐标是（0,0），右下角坐标为（1024,768）。转换函数如下：
+            int ScreenHeight = Screen.PrimaryScreen.WorkingArea.Height;
+            int ScreenWidth = Screen.PrimaryScreen.WorkingArea.Width;
+            int x = barRect.Left + (IconRect.Left / 2);
+            int y = barRect.Top + (IconRect.Top / 2);
+            int dx = x * (65335 / ScreenWidth); //x,y为像素坐标。
+            int dy = y * (65335 / ScreenHeight);//ScreenWidth和ScreenHeight，其实是当前显示器的分辨率，获得方法是ScreenWidth=Screen.PrimaryScreen.WorkingArea.Width；
+
+            MouseInput myMinput = new MouseInput();
+            myMinput.dx = i;
+            myMinput.dy = j;
+            myMinput.Mousedata = 0;
+            myMinput.dwFlag = MouseEvent_Absolute | MouseEvent_Move | MouseEvent_LeftDown | MouseEvent_LeftUp;
+            myMinput.time = 0;
+            Input[] myInput = new Input[1];
+            myInput[0] = new Input();
+            myInput[0].type = 0;
+            myInput[0].mi = myMinput;
+            UInt32 result = SendInput((uint)myInput.Length, myInput, Marshal.SizeOf(myInput[0].GetType()));
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bak();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+        }
+
+        private void 打开主窗体ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+
+        private void 退出程序ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("您确定要退出程序，退出后备份会不正常工作！", "警告", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                System.Environment.Exit(0);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetAutoRun();
+            this.notifyIcon1.ShowBalloonTip(2000, "提示", "备份服务开始运行！", ToolTipIcon.Info);
+        }
+        private void SetAutoRun()
+        {
+            try
+            {
+                string path = Application.ExecutablePath;
+                RegistryKey rk = Registry.LocalMachine;
+                RegistryKey rk2 = rk.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run");
+                rk2.SetValue("BaiduYunBakTool", path + " /h");
+                rk2.Close();
+                rk.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Log("SetAutoRun", ex.Message);
+                MessageBox.Show(ex.Message);
+            }
+        }
+        /// <summary>
+        /// 添加文本日志
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="msg"></param>
+        public void Log(string type, string msg)
+        {
+            try
+            {
+                var dir = AppDomain.CurrentDomain.BaseDirectory + "\\log";
+                var log = dir + "\\" + System.DateTime.Now.ToString("yyyyMMdd") + "log.txt";
+                if (!Directory.Exists(dir))
+                {
+                    Directory.CreateDirectory(dir);
+                }
+                if (File.Exists(log))
+                {
+                    var file = new FileInfo(log);
+                    if (file.Length >= 1024 * 1024 * 10) //100M
+                    {
+                        file.Delete();
+                    }
+                }
+                File.AppendAllText(log, "\r\n时间:" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "操作：" + type + "\r\n内容：" + msg);
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
